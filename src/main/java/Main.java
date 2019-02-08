daeb9417bb8f232fc6ff875c708fc885459a6ba3
Matthew Swahn <matthewswahn@gmail.com>
2019-02-07 23:17:28 -0500

    Change file input to use file path instead of hard coding filepath.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import GMMObjects.gaussianMixtureModel;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -7,11 +8,12 @@ import static FuncsAndUtils.ImportUtils.GMMFromCSV;
 
 public class Main {
     public static void main(String[] args) {
-        String filepath = "C:/Users/matthew.swahn/Documents/IdeaProjects/TMCProjects/" +
-                "mooc-2013-OOProgrammingWithJava-PART1/gaussianMixtureModel-EM.project/src/main/resources/gmm-data.csv";
-        ArrayList<Double> initValues = new ArrayList<>(Arrays.asList(13.4, 27.0, -4.0));
+        String filePathBase = new File("").getAbsolutePath();
+        String filePath = filePathBase.concat("/src/main/resources/gmm-data.csv");
+        System.out.println("filepath: " + filePath);
+        ArrayList<Double> initValues = new ArrayList<>(Arrays.asList(13.4, 27.0));
         //in test file we know we have a N(1,3) with weight 40% and a N(30,4) 60%
-        gaussianMixtureModel Test1 = GMMFromCSV(filepath,
+        gaussianMixtureModel Test1 = GMMFromCSV(filePath,
                 false,
                 initValues,
                 500,
