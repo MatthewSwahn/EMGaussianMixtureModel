8ba974e8436dd0b322b3d0ffd16dc67b2e7e4373
Matthew Swahn <matthewswahn@gmail.com>
2019-02-07 23:18:25 -0500

    remove print statement to check filepath

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         String filePathBase = new File("").getAbsolutePath();
         String filePath = filePathBase.concat("/src/main/resources/gmm-data.csv");
-        System.out.println("filepath: " + filePath);
+
         ArrayList<Double> initValues = new ArrayList<>(Arrays.asList(13.4, 27.0));
         //in test file we know we have a N(1,3) with weight 40% and a N(30,4) 60%
         gaussianMixtureModel Test1 = GMMFromCSV(filePath,
