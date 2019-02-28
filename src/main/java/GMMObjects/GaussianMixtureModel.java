ab573bdcf69e10b29571f1c6a5a9546e053e298a
Matthew Swahn <matthewswahn@gmail.com>
2019-02-27 23:48:25 -0500

    fix multidimensional gmm branch

--- src/main/java/GMMObjects/GaussianMixtureModel.java
+++ src/main/java/GMMObjects/GaussianMixtureModel.java
@@ -7,7 +7,6 @@ import org.apache.commons.math3.linear.RealMatrix;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Vector;
 
 import static FuncsAndUtils.ArrayUtilities.*;
 
