1e194c895d7ffee8e1c472f6a7a370b32d5b313f
Matthew Swahn <matthewswahn@gmail.com>
2019-03-01 09:15:56 -0500

    update data notes and change example to test multidimensional GMM

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,14 +9,17 @@ import static FuncsAndUtils.ImportUtils.GMMFromCSV;
 public class Main {
     public static void main(String[] args) {
         String filePathBase = new File("").getAbsolutePath();
-        String filePath = filePathBase.concat("/src/main/resources/gmm-data.csv");
+        String filePath = filePathBase.concat("/src/main/resources/multigmm-data.csv");
 
-        ArrayList<Double> initValues = new ArrayList<>(Arrays.asList(13.4, 27.0));
+        ArrayList<double[]> initValues = new ArrayList<>(Arrays.asList(
+                new double[]{1.2, 3.14}, new double[]{10.5, 27.0}));
+        System.out.println("initValues size: " + initValues.size());
         //in test file we know we have a N(1,3) with weight 40% and a N(30,4) 60%
         GaussianMixtureModel Test1 = GMMFromCSV(filePath,
-                false);
+                true);
         Test1.EMGMM(initValues, 500, 1e-8);
 
         System.out.println("Components are: " + Test1.getComponentValues());
+        System.out.println("Components size: " + Test1.components.size());
     }
 }
