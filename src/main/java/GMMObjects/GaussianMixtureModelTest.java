279e2677fda97738ec79eabfa992139493a38c30
Matthew Swahn <matthewswahn@gmail.com>
2019-03-12 23:08:58 -0400

    update gradle to include junit 5, add bare bones junit tests for GaussianMixtureModel

---
+++ src/main/java/GMMObjects/GaussianMixtureModelTest.java
@@ -0,0 +1,38 @@
+package GMMObjects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GaussianMixtureModelTest {
+
+    @org.junit.jupiter.api.Test
+    void getComponents() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getComponentValues() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void EStepDatum() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void EStep() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void meansMStep() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void covMStep() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void MStep() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void GMMLogLikelihood() {
+    }
+}
