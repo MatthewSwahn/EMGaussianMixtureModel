3289f1167e348d9bc211d121aa817db34ca6a681
Matthew Swahn <matthewswahn@gmail.com>
2019-03-21 00:19:10 -0400

    comment test example more clearly in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,9 @@ public class Main {
         ArrayList<double[]> initValues = new ArrayList<>(Arrays.asList(
                 new double[]{1.2, 3.14}, new double[]{10.5, 27.0}));
         System.out.println("initValues size: " + initValues.size());
-        //in test file we know we have a N(1,3) with weight 40% and a N(30,4) 60%
+        //in muligmm-data test file, we have
+        // component 1: 40% with mean = [2, 5] and cov = [[1, .5], [.5, 100]]
+        // component 2: 60% with mean = [6, 9] and cov = [[5, 3], [3, 40]]
         GaussianMixtureModel Test1 = GMMFromCSV(filePath,
                 true);
         Test1.fitGMM(initValues, 500, 1e-8);
