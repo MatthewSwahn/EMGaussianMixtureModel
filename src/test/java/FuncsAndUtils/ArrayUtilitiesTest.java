efcca2d27f28558140360dbb02dce74ecdbfec5d
Matthew Swahn <matthewswahn@gmail.com>
2019-03-25 23:11:03 -0400

    add ArrayUtilitiesTest file

---
+++ src/test/java/FuncsAndUtils/ArrayUtilitiesTest.java
@@ -0,0 +1,22 @@
+package FuncsAndUtils;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayUtilitiesTest {
+
+    @Test
+    void l1Norm() {
+    }
+
+    @Test
+    void euclidDist() {
+    }
+
+    @Test
+    void distToCenterL1() {
+    }
+}
